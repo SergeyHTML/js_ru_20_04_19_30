9d051389b1ae1cf82a008c4eef8715ab66651691
Roman <kaboom.ua@gmail.com>
2017-05-22 14:46:00 +0300

    Update randomId.js

--- src/middlewares/randomId.js
+++ src/middlewares/randomId.js
@@ -1,5 +1,6 @@
 export default store => next => action => {
     const {...rest} = action
+    //через мидлвары будет проходить каждый экшин, но не каждому нужно генерить id
     next({
         ...rest,
         randomId: Date.now()
